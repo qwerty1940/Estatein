4d32c50b463dab0fd64ffd52efbd38bfee097cbe
qwerty1940 <t.j.910071@gmail.com>
2025-06-10 15:27:01 +0100

    Add files via upload
    my first project

---
+++ My_first_project/asset/js/index.js
@@ -0,0 +1,475 @@
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container2");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(" .container3 #c31");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(" .container3 #c32");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(" #cont4 #c31");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(" #cont4 #c32");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(" .container4 #c63");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container4 #c62");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container4 #c65");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 #alpha1");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 input");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha7 #alpha70");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha7 #alpha71");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha7 #alpha72");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha7 #alpha73");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha7 #alpha74");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha7 #alpha75");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha8 #alpha76");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha8 #alpha77");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha8 #alpha78");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha8 #alpha79");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha8 #alpha80");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha8 #alpha81");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha9 #alpha82");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha9 #alpha83");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha9 #alpha84");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha10 #alpha85");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha10 #alpha86");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha10 #alpha87");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha10 #alpha88");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha10 #alpha89");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha10 #alpha90");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show"); 
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha11 #alpha91");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha11 #alpha92");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const box = document.querySelector(".container5 .alpha11 #alpha93");
+  const boxTop = box.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (boxTop < windowHeight - 100) {
+    box.classList.add("show");
+  } else {
+    box.classList.remove("show");
+  }
+});
+
+  window.addEventListener('load', () => {
+    const counter = document.querySelector('.counter');
+    const target = +counter.getAttribute('data-target');
+    let count = 0;
+
+    const updateCount = () => {
+      const increment = target / 100;
+      if (count < target) {
+        count += increment;
+        counter.textContent = Math.ceil(count);
+        requestAnimationFrame(updateCount);
+      } else {
+        counter.textContent = target;
+      }
+    };
+
+    updateCount();
+  });
+
+    window.addEventListener('load', () => {
+    const counter = document.querySelector('.counter1');
+    const target = +counter.getAttribute('data-target');
+    let count = 0;
+
+    const updateCount = () => {
+      const increment = target / 100;
+      if (count < target) {
+        count += increment;
+        counter.textContent = Math.ceil(count);
+        requestAnimationFrame(updateCount);
+      } else {
+        counter.textContent = target;
+      }
+    };
+
+    updateCount();
+  });
+
+    window.addEventListener('load', () => {
+    const counter = document.querySelector('.counter2');
+    const target = +counter.getAttribute('data-target');
+    let count = 0;
+
+    const updateCount = () => {
+      const increment = target / 100;
+      if (count < target) {
+        count += increment;
+        counter.textContent = Math.ceil(count);
+        requestAnimationFrame(updateCount);
+      } else {
+        counter.textContent = target;
+      }
+    };
+
+    updateCount();
+  });
+
+
+  document.querySelector('i').addEventListener('click',()=>{
+      const side=document.querySelector('.sidebar');
+    side.classList.toggle('show1')
+  })
+
+
